2b775f789e0504b5fc60effa47c3aa849ccf498b
rahulsharma1902 <rahulsharma1902@gmail.com>
2025-12-26 12:30:02 +0530

    🧩 Practice: Algorithm: Linked List

---
+++ algorithms/2025-12/linked_list_123001.py
@@ -0,0 +1,49 @@
+"""
+Singly Linked List Implementation
+"""
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+    
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        return elements
+    
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+# Test
+if __name__ == "__main__":
+    ll = LinkedList()
+    for i in [1, 2, 3, 4, 5]:
+        ll.append(i)
+    print(f"List: {ll.display()}")
+    ll.reverse()
+    print(f"Reversed: {ll.display()}")
